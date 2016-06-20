bf82d7de0348033367b645ace9dff8c7b1e96480
shminer <a332574643@gmail.com>
2016-06-20 14:28:29 +0800

    Support mokee cm stock rom, add .gitignore

--- tmp/amount.sh
+++
@@ -1,7 +0,0 @@
-#!/tmp/busybox sh
-
-BB=/tmp/busybox
-
-systemfs=$(eval $($BB blkid /dev/block/platform/msm_sdcc.1/by-name/system | $BB awk ' { print $3 } '); $BB echo $TYPE);
-
-$BB mount -t $systemfs /dev/block/platform/msm_sdcc.1/by-name/system /system;
